97ad68d4d1041501b0a81e260daee1d1cfe6bfdc
John Kersey <johnkersey2@yahoo.com>
2023-06-02 20:48:03 -0400

    Added config code to connect.

--- models/User.js
+++ models/User.js
@@ -13,8 +13,8 @@ const userSchema = new Schema(
             unique: true,
             required: true,
             trim: true,
-            validate: [validateEmail, 'Please fill a valid email address'],
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+            validate: [validateEmail, 'Please enter a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
         },
         thoughts: [{
             type: Schema.Types.Array,
