02ad28da6c165952879664b16914d1bc41181ad8
John Kersey <johnkersey2@yahoo.com>
2023-06-04 20:12:13 -0400

    Added API routes.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getThought,
+  getSinglethought,
+  createThought,
+  deleteThought,
+  updateThought,
+  addReaction,
+  removeReaction, 
+  removeThought
+  
+} = require('../../controller/thoughtcontroller');
+
+router.route('/').get(getThought).post(createThought);
+
+router
+  .route('/:id')
+  .get(getSinglethought)  
+  .delete(deleteThought);
+
+router.route("/addReaction/:thoughtId").put(addReaction);
+
+router.route("/removeReaction/:thoughtId/:reactionId").delete(removeReaction);
+
+router.route("/:userId/thoughts/:thoughtId").delete(removeThought);
+
+router.route("/:id/thoughts").put(updateThought);
+
+module.exports = router;
